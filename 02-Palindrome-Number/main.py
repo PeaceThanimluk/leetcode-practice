1f9f8e57b6b565172f86b959e80da4299c4e3fe5
Supatcharanan <supatcharanan.thanimluk@gmail.com>
2026-07-02 02:20:27 +0700

    🟨💀💀

---
+++ 02-Palindrome-Number/main.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x : int) -> bool:
+        s = str(x)
+        return s == s[::-1] #กลับลำดับ
+    
+newSolution = Solution()
+
+print(newSolution.isPalindrome(1001))
