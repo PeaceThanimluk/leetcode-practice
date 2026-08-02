d5dbff5fbb8a0abbc35fc25c5b53b38b641d1abf
peace <supatcharanan.thanimluk@gmail.com>
2026-08-03 01:22:19 +0700

    femboy

---
+++ Section/02-TwoPointer/Container-With-Most-Water.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def maxArea(self, height):
+        left = 0
+        right = len(height) - 1
+        max_area = 0
+
+        while left < right:
+            width = right - left
+            current_height = min(height[left], height[right])
+            area = width * current_height
+
+            if area > max_area:
+                max_area = area
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_area
+
+newSolution = Solution()
+print(newSolution.maxArea([1,8,6,2,5,4,8,3,7]))
+        
